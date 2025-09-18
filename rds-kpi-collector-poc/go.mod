648cc066bce490a7e38cc591195a7ef27b3b8297
yaronh12 <yaron.hod1@gmail.com>
2025-09-18 17:11:09 +0300

    Successfully get data with golang prometheus client, and flags for toke, url and kubeconfig

---
+++ rds-kpi-collector-poc/go.mod
@@ -0,0 +1,15 @@
+module rds-kpi-collector
+
+go 1.24.6
+
+require github.com/prometheus/client_golang v1.23.2
+
+require (
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.66.1 // indirect
+	go.yaml.in/yaml/v2 v2.4.2 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
+)
--- rds-kpi-collector-poc/main.go
+++ rds-kpi-collector-poc/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 )
 
 const (
@@ -24,6 +31,11 @@ type PrometheusResponse struct {
 		ResultType string `json:"resultType"`
 	} `json:"data"`
 	Status string `json:"status"`
+}
+
+type QueryResponse struct {
+	PrometheusResponse PrometheusResponse `json:"prometheus_response"`
+	ErrorMsg           string             `json:"error"`
 }
 
 type KPIs struct {
@@ -48,16 +60,28 @@ func main() {
 	// Option 2: --kubeconfig
 
 	var (
-		bearerToken = flag.String("token", "", "bearer token for thanos-queries")
-		thanosURL   = flag.String("thanos-url", "", "thanos url for http requests")
-		kubeconfig  = flag.String("kubeconfig", "", "kubeconfig file path")
+		bearerToken string
+		thanosURL   string
+		kubeconfig  string
 	)
+
+	flag.StringVar(&bearerToken, "token", "", "bearer token for thanos-queries")
+	flag.StringVar(&thanosURL, "thanos-url", "", "thanos url for http requests")
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "kubeconfig file path")
+	// validateFlags checks that either both token and thanos-url are provided,
+	// or kubeconfig is provided, but not both scenarios
 
 	flag.Parse()
 
-	fmt.Println("bearer token: ", *bearerToken)
-	fmt.Println("thanos url: ", *thanosURL)
-	fmt.Println("kubeconfig: ", *kubeconfig)
+	fmt.Println("bearer token: ", bearerToken)
+	fmt.Println("thanos url: ", thanosURL)
+	fmt.Println("kubeconfig: ", kubeconfig)
+
+	err := validateFlags(bearerToken, thanosURL, kubeconfig)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
 
 	// Open and read the kpis.json file
 	kpisFile, err := os.Open("kpis.json")
@@ -77,19 +101,19 @@ func main() {
 
 	// Print the queries from the predefined file
 	var queries []string
-	fmt.Printf("kpis queries as Go struct:\n")
+	//fmt.Printf("kpis queries as Go struct:\n")
 	for _, query := range kpis.Queries {
 		queries = append(queries, query.PromQuery)
 	}
-	fmt.Println(queries)
+	//fmt.Println(queries)
 	// We run the commands
-	queriesResults, err := runQueries(queries)
+	queriesResults, err := runQueries(queries, thanosURL, bearerToken)
 	if err != nil {
 		fmt.Printf("Failed to run commands: %v\n", err)
 		return
 	}
 
-	fmt.Println(queriesResults)
+	fmt.Printf("\n\n\n\n%v", queriesResults)
 
 	// // We save the results to a file
 	// err = saveToFile(commandsResults, OUTPUT_FILE)
@@ -102,13 +126,94 @@ func main() {
 
 }
 
-func runQueries(queriesToRun []string) (map[string]PrometheusResponse, error) {
-	// this is a map: key is the query, output of the query is the value
-	queriesResults := make(map[string]PrometheusResponse)
+func validateFlags(token string, url string, kubeconfig string) error {
+	if (token != "" && url != "" && kubeconfig == "") ||
+		(token == "" && url == "" && kubeconfig != "") {
+		return nil
+	} else {
+		return fmt.Errorf("invalid flag combination: either provide --token and --thanos-url, or provide --kubeconfig")
+	}
+}
 
-	for _, command := range queriesToRun {
-		fmt.Printf("Running: %s\n", command)
-		// We run the command and get the output
+func runQueries(queriesToRun []string, thanosURL string, bearerToken string) (map[string]QueryResponse, error) {
+	// this is a map: key is the query, output of the query is the value
+	queriesResults := make(map[string]QueryResponse)
+	// tr := &http.Transport{
+	// 	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	// }
+	// client := &http.Client{Transport: tr}
+
+	// Create Prometheus client
+	client, err := api.NewClient(api.Config{
+		Address: "https://" + thanosURL,
+		RoundTripper: &tokenRoundTripper{
+			token: bearerToken,
+			rt: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+			// instead of - api.DefaultRoundTripper
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create client: %v", err)
+	}
+
+	v1api := v1.NewAPI(client)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	for _, query := range queriesToRun {
+		fmt.Println("------------------------")
+		fmt.Printf("Running: %s\n", query)
+		// Create POST request with form data
+		// data := url.Values{}
+		// data.Set("query", query)
+		// req, err := http.NewRequest("POST", "https://"+(thanosURL)+"/api/v1/query", strings.NewReader(data.Encode()))
+		// if err != nil {
+		// 	continue
+		// }
+		// Execute query using the client library
+		result, warnings, err := v1api.Query(ctx, query, time.Now())
+		if err != nil {
+			fmt.Println("query failed: ", err)
+			queriesResults[query] = QueryResponse{ErrorMsg: fmt.Sprintf("query failed: %v", err)}
+			continue
+		}
+
+		if len(warnings) > 0 {
+			fmt.Printf("Warnings: %v\n", warnings)
+		}
+
+		fmt.Println("RESULT:", result)
+
+		// // Add bearer token
+		// req.Header.Set("Authorization", "Bearer "+(bearerToken))
+		// req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		// resp, err := client.Do(req)
+		// if err != nil {
+		// 	fmt.Printf("Request failed: %v\n", err)
+		// 	continue
+		// }
+		// defer resp.Body.Close()
+
+		// // Read the response body
+		// body, err := io.ReadAll(resp.Body)
+		// if err != nil {
+		// 	fmt.Printf("Failed to read response: %v\n", err)
+		// 	continue
+		// }
+
+		// // Print the actual JSON
+		// var prettyJSON interface{}
+		// if err := json.Unmarshal(body, &prettyJSON); err == nil {
+		// 	formatted, _ := json.MarshalIndent(prettyJSON, "", "  ")
+		// 	fmt.Printf("Response JSON:\n%s\n", string(formatted))
+		// } else {
+		// 	fmt.Printf("Raw response: %s\n", string(body))
+		// }
+		//fmt.Println(resp)
+		fmt.Println("------------------------")
 
 	}
 
@@ -129,3 +234,17 @@ func saveToFile(data map[string]PrometheusResponse, filename string) error {
 	}
 	return nil
 }
+
+// Custom RoundTripper to add Bearer token
+type tokenRoundTripper struct {
+	token string
+	rt    http.RoundTripper
+}
+
+func (t *tokenRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("Authorization", "Bearer "+t.token)
+	return t.rt.RoundTrip(req)
+}
+
+//TOKEN = eyJhbGciOiJSUzI1NiIsImtpZCI6Im5mOTl3a05TN2dCQ19Zdl8yOExsc1hBUE9vR2pIR19fZnQxemY5RHk2Y0UifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjIl0sImV4cCI6MTc1ODIyNzM5NiwiaWF0IjoxNzU4MTkxMzk2LCJpc3MiOiJodHRwczovL2t1YmVybmV0ZXMuZGVmYXVsdC5zdmMiLCJqdGkiOiI4MjZkMWQyOS1lZTkwLTQ2Y2QtYjgxNy04ZDExOGY1OTY5ODUiLCJrdWJlcm5ldGVzLmlvIjp7Im5hbWVzcGFjZSI6Im9wZW5zaGlmdC1tb25pdG9yaW5nIiwic2VydmljZWFjY291bnQiOnsibmFtZSI6InRlbGVtZXRlci1jbGllbnQiLCJ1aWQiOiJiNDM3ODYzMS1mNWQ3LTQxMGYtYWI5OS1kOTM4ZDgyMGY3ZGQifX0sIm5iZiI6MTc1ODE5MTM5Niwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50Om9wZW5zaGlmdC1tb25pdG9yaW5nOnRlbGVtZXRlci1jbGllbnQifQ.HnFq-3x9MMuHrL7OAOZzsWUxhCNM1k9ULvJxMunMmVt8hWKwOd6O_gBNM6EYxDWvWFoqxPIq0ItpwoXY6oESMBv7BXMYCkiby5JvKJYVdqJmrkJuW59LPA8IRGWMBkQHRFu7yph8LMAKfOwzjfzm9Cwnwj3WzmZMkhFWJmvl1YxvOopTxHdWvLPddioCcLtXBqY6d0rwWyS2hCIFkAQt29kPuCT2IY1FYZvhhuA8J-Yhg8IuR52dmvWlS7tjFxW5O6WqpTv5RNZK2jHFPDCCp1SI70iqjOzkdjEleyPZdTbAaro3nPXf6BAK1a3y7WDnKtlV64NbZrhxUxadTan_en5p4TKfHpIuTSOiq6XqbF4w4TBhTHnb0aWClhch5LKFNTwViwmgabL7g0vJJPG05zNIHtKK5xfVC0Uls9MuBKIlBpdjh8d9MEK7us1qt49Tguby6OPxgQjsbb3riF95LpuOapM1lPzCQVnkP9NqChbPUEo0U0ox6bbmikObXya7-Z5_E1dGF4lEn6iphaQ-TerNLLCp0ZYo46S55bCt8zncWuBSRkEmdl1U18lo-6Mo2otjOybjQP49bj9FfCvJv_Uk_wTps7e8pkvUPgqJhAg11au1kMlWzUQYUu0p6ekzFp42DuP6wsErPAggh3RMJVsJcn3q1sMXl6qi5RgSxOs
+//thanor querier url = thanos-querier-openshift-monitoring.apps.clus0.t5g.lab.eng.rdu2.redhat.com
